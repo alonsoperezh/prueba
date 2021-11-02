787a2946635d9dce25186ebd2b97c82ff39be2c0
alonsoperezh <alonsos2001@hotmail.com>
2021-11-02 18:46:34 +0100

    ecija la shupa

---
+++ cosa/cosa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cosa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ECija la chupa!);
+        }
+    }
+}
